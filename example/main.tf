a5edcf43c446f5d681d0a04e1a87d5740e104786
Łukasz Żułnowski <lzulnowski@gmail.com>
2019-11-04 18:35:59 +0100

    change module source

--- example/main.tf
+++ example/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "us-east-1"
 }
 module "blog" {
-  source = "../"
+  source = "github.com/buker/blog-terraform-aws"
   bucket_name = "demo-blog.zulnowski.com"
   domain_name = "demo-blog.zulnowski.com"
 }
